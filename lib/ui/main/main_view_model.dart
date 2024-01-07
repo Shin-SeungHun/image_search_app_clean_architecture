59d1c7f210ce33ac8d57645784a58dadfa965f3f
shin-SeungHun <sng7387@gmail.com>
2024-01-08 07:51:35 +0900

    viewModel에서 메인화면으로 데이터 전달

---
+++ lib/ui/main/main_view_model.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:image_search_app_clean_architecture/core/result.dart';
+import 'package:image_search_app_clean_architecture/data/model/image_item.dart';
+import 'package:image_search_app_clean_architecture/data/repository/image_item_repository.dart';
+import 'package:image_search_app_clean_architecture/ui/main/main_state.dart';
+
+import 'main_event.dart';
+
+class MainViewModel extends ChangeNotifier {
+  final ImageItemRepository _imageItemRepository;
+
+  MainState _state = const MainState();
+
+  MainState get state => _state;
+
+  final _eventController = StreamController<MainEvent>();
+
+  Stream<MainEvent> get eventStream => _eventController.stream;
+
+  MainViewModel({
+    required ImageItemRepository imageItemRepository,
+  }) : _imageItemRepository = imageItemRepository;
+
+  Future<void> fetchImages(String query) async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+    final result = await _imageItemRepository.getImageItems(query);
+    switch (result) {
+      case Success<List<ImageItem>>():
+        _state = state.copyWith(
+          isLoading: false,
+          imageItems: result.data,
+        );
+        notifyListeners();
+      case Error<List<ImageItem>>():
+        _state = state.copyWith(
+          isLoading: false,
+          imageItems: [],
+        );
+        notifyListeners();
+        _eventController.add(const MainEvent.dataLoadingError());
+    }
+  }
+}
