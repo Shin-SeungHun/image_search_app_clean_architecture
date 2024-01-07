746766dc58e517c1fc5fcf2e678e9e945e0a4959
shin-SeungHun <sng7387@gmail.com>
2024-01-08 07:55:29 +0900

    refactor : go_router

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,6 +1,8 @@
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import '../data/model/image_item.dart';
 import '../di/di_setup.dart';
+import '../ui/detail/detail_screen.dart';
 import '../ui/main/main_screen.dart';
 import '../ui/main/main_view_model.dart';
 
@@ -14,5 +16,11 @@ final router = GoRouter(
         child: const MainScreen(),
       ),
     ),
+    GoRoute(
+        path: '/detailScreen',
+        builder: (context, state) {
+          final imageItem = state.extra as ImageItem;
+          return DetailScreen(imageItem: imageItem);
+        }),
   ],
 );
