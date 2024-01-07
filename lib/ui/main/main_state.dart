a839cdffb6c48d9aec3159041e496cbbc518defd
shin-SeungHun <sng7387@gmail.com>
2024-01-08 07:50:28 +0900

    state로 view의 상태관리

---
+++ lib/ui/main/main_state.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../data/model/image_item.dart';
+
+part 'main_state.freezed.dart';
+
+part 'main_state.g.dart';
+
+@freezed
+sealed class MainState<T> with _$MainState<T> {
+  const factory MainState({
+    @Default(false) bool isLoading,
+    @Default([]) List<ImageItem> imageItems,
+  }) = _MainState;
+
+  factory MainState.fromJson(Map<String, Object?> json) =>
+      _$MainStateFromJson(json);
+}
