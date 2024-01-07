86089b1fc713ab4bc480d9eb1055ccbdae24f84d
shin-SeungHun <sng7387@gmail.com>
2024-01-08 06:49:36 +0900

    repository interface 작성

---
+++ lib/data/repository/image_item_repository.dart
@@ -0,0 +1,6 @@
+import '../../core/result.dart';
+import '../model/image_item.dart';
+
+abstract interface class ImageItemRepository {
+  Future<Result<List<ImageItem>>> getImageItems(String query);
+}
