1cd881a41ea1b5e07ddc3ab441690d0cf99fcd26
shin-SeungHun <sng7387@gmail.com>
2024-01-08 07:11:40 +0900

    DI 작성

---
+++ lib/di/di_setup.dart
@@ -0,0 +1,14 @@
+import 'package:get_it/get_it.dart';
+import 'package:image_search_app_clean_architecture/data/repository/image_item_repository.dart';
+import 'package:image_search_app_clean_architecture/data/repository/image_item_repository_impl.dart';
+
+import '../data/data_source/image_api.dart';
+
+final getIt = GetIt.instance;
+
+void diSetup() {
+  getIt.registerSingleton<ImageApi>(ImageApi());
+
+  getIt.registerSingleton<ImageItemRepository>(
+      ImageItemRepositoryImpl(api: getIt<ImageApi>()));
+}
